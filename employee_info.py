1c4cea65d8fccc20a56bf1ccd71252d3d70397a3
JJunJia <jjjunjia97.23@ichat.sp.edu.sg>
2024-05-04 22:40:52 +0800

    Edited 2 functions

--- employee_info.py
+++ employee_info.py
@@ -21,17 +21,17 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 def calculate_average_salary():
     total = 0
     average = 0
-
-    #add your implementation to calculate here
-
-
+    for item in employee_data:
+        total=total+item["salary"]
+    average="{:.2f}".format(total/len(employee_data))
     return average
 
 def get_employees_by_dept(department):
     result = []
-
-    # Add your implementation from here
-
+    
+    for item in employee_data:
+        if item["department"] ==  department:
+            result.append(item)
 
     return result
 
